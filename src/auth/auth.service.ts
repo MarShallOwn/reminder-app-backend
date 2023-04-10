72c23d4010e9137d49d1edb049b712fb0b4836bc
MarShallOwn <marwansamih15@gmail.com>
2023-04-10 02:51:11 +0200

    connected mongodb prisma with our application

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
 
 
 @Injectable({})
 export class AuthService {
+    constructor(private prisma: PrismaService) {}
+
     signin() {
+        console.log("Hit the backend")
         return {msg: "Testing Signin"}
     }
 
